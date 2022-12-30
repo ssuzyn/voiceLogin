f19c9f3b8a27bce45bf9963b3b464a090753db1b
ssuzyn <asj7090@gmail.com>
2022-12-30 13:31:17 +0900

    음성녹음 uploads파일에 저장 성공

--- server.py
+++ server.py
@@ -2,68 +2,40 @@ from flask import Flask, request, render_template, redirect, url_for
 from flaskext.mysql import MySQL
 from werkzeug.utils import secure_filename
 import os
+import logging
 
 mysql = MySQL()
 app = Flask(__name__)
 
+
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'annie1004'
 app.config['MYSQL_DATABASE_DB'] = 'voice'
-
 mysql.init_app(app)
 
 @app.route("/", methods=['POST', 'GET'])
 def main():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/upload', methods = ['GET', 'POST'])
-def file_upload():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save('static/uploads/' + secure_filename(f.filename))
+@app.route("/login", methods=['POST', 'GET'])
+def signUp():
+    return render_template("signup.html")
+
+@app.route("/signup", methods=['POST', 'GET'])
+def signup():
+    if(request.method=='POST'):
+        data = request.files['audio_data']
+        print(data.read())
+        data.save('static/uploads/' + secure_filename(data.filename))
         files = os.listdir("static/uploads")
- 
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        # 파일명과 파일경로를 데이터베이스에 저장함
-        sql = "INSERT INTO records (name, dir) VALUES ('%s', '%s')" % (secure_filename(f.filename), 'uploads/'+secure_filename(f.filename))
-        cursor.execute(sql)
-        data = cursor.fetchall()
- 
-        if not data:
-            conn.commit()
-            return redirect(url_for('main'))
- 
-        else:
-            conn.rollback()
-            return 'upload failed'
-        
-        cursor.close()
-        conn.close()
+        return render_template("check.html")
+    else:
+        return redirect(url_for('main'))
 
-@app.route('/view', methods = ['GET', 'POST'])
-def view():
-    conn = mysql.connect()  # DB와 연결
-    cursor = conn.cursor()  # connection으로부터 cursor 생성 (데이터베이스의 Fetch 관리)
-    sql = "SELECT name, dir FROM records"  # 실행할 SQL문
-    cursor.execute(sql)  # 메소드로 전달해 명령문을 실행
-    data = cursor.fetchall()  # 실행한 결과 데이터를 꺼냄
- 
-    data_list = []
- 
-    for obj in data:  # 튜플 안의 데이터를 하나씩 조회해서
-        data_dic = {  # 딕셔너리 형태로
-            # 요소들을 하나씩 넣음
-            'name': obj[0],
-            'dir': obj[1]
-        }
-        data_list.append(data_dic)  # 완성된 딕셔너리를 list에 넣음
- 
-    cursor.close()
-    conn.close()
- 
-    return render_template('view.html', data_list=data_list)  # html을 렌더하며 DB에서 받아온 값들을 넘김
- 
+@app.route("/upload", methods=['POST', 'GET'])
+def file_upload():
+    return render_template("check.html")
+    
 
 if __name__=="__main__":
   app.run(debug=True)
--- static/js/record.js
+++ static/js/record.js
@@ -1,154 +1,79 @@
-const record = document.getElementById("record")
-const stop = document.getElementById("stop")
-const soundClips = document.getElementById("sound-clips")
-const chkHearMic = document.getElementById("chk-hear-mic")
+// 엘리먼트 취득
+const $audioEl = document.querySelector("audio");
+const signup = document.querySelector("button")
+const pwd = document.getElementById("pwd");
+const id = document.getElementById("id")
 
-//음원정보를 담은 노드를 생성하거나 음원을 실행 또는 디코딩
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)() // 오디오 컨텍스트 정의
+// 녹음중 상태 변수
+let isRecording = false;
 
-const analyser = audioCtx.createAnalyser()
+// MediaRecorder 변수 생성
+let mediaRecorder = null;
 
-function makeSound(stream) { //내 컴퓨터의 마이크나 다른 소스를 통해 발생한 오디오 스트림의 정보를 보여줌
-    const source = audioCtx.createMediaStreamSource(stream)
-    source.connect(analyser)
-    analyser.connect(audioCtx.destination)
-}
+// 녹음 데이터 저장 배열
+const audioArray = [];
 
-if (navigator.mediaDevices) { //마이크 사용 권한 획득
-    console.log('getUserMedia supported.')
+let blob;
 
-    const constraints = {
-        audio: true
+pwd.onclick = async function (event) {
+    if (!isRecording) {
+
+        // 마이크 mediaStream 생성: Promise를 반환하므로 async/await 사용
+        const mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+        // MediaRecorder 생성
+        mediaRecorder = new MediaRecorder(mediaStream);
+
+        // 이벤트핸들러: 녹음 데이터 취득 처리
+        mediaRecorder.ondataavailable = (event) => {
+            audioArray.push(event.data); // 오디오 데이터가 취득될 때마다 배열에 담아둔다.
+            Object.keys(event.data).forEach(key => {
+                console.log(key);
+                console.log(audioArray[key])
+            })
+
+        }
+
+        // 이벤트핸들러: 녹음 종료 처리 & 재생하기
+        mediaRecorder.onstop = (event) => {
+            // 녹음이 종료되면, 배열에 담긴 오디오 데이터(Blob)들을 합친다: 코덱도 설정해준다.
+            blob = new Blob(audioArray, { "type": "audio/ogg codecs=opus" });
+            audioArray.splice(0); // 기존 오디오 데이터들은 모두 비워 초기화한다.
+
+            // Blob 데이터에 접근할 수 있는 주소를 생성한다.
+            const blobURL = window.URL.createObjectURL(blob);
+
+            // audio엘리먼트로 재생한다.
+            $audioEl.src = blobURL;
+            console.log(blob)
+            $audioEl.play();
+
+        }
+
+        // 녹음 시작
+        mediaRecorder.start();
+        isRecording = true;
+
+    } else {
+        // 녹음 종료
+        mediaRecorder.stop();
+        isRecording = false;
     }
-    let chunks = []
-
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then(stream => {
-
-            const mediaRecorder = new MediaRecorder(stream)
-
-            chkHearMic.onchange = e => {
-                if (e.target.checked == true) {
-                    audioCtx.resume()
-                    makeSound(stream)
-                } else {
-                    audioCtx.suspend()
-                }
-            }
-
-            record.onclick = () => {
-                mediaRecorder.start()
-                console.log(mediaRecorder.state)
-                console.log("recorder started")
-                record.style.background = "red"
-                record.style.color = "black"
-            }
-
-            stop.onclick = () => {
-                mediaRecorder.stop()
-                console.log(mediaRecorder.state)
-                console.log("recorder stopped")
-                record.style.background = ""
-                record.style.color = ""
-                //mediaRecorder.exportWAV(createDownloadLink)
-            }
-
-            mediaRecorder.onstop = e => {
-                console.log("data available after MediaRecorder.stop() called.")
-
-                const clipName =  new Date().toISOString();
-
-                const clipContainer = document.createElement('article')
-                const clipLabel = document.createElement('p')
-                const audio = document.createElement('audio')
-                const deleteButton = document.createElement('button')
-                const uploadButton = document.createElement('button')
-
-
-                clipContainer.classList.add('clip')
-                audio.setAttribute('controls', '')
-                deleteButton.innerHTML = "삭제"
-                uploadButton.innerHTML = "업로드"
-                clipLabel.innerHTML = clipName
-
-                clipContainer.appendChild(audio)
-                clipContainer.appendChild(clipLabel)
-                clipContainer.appendChild(deleteButton)
-                clipContainer.appendChild(uploadButton)
-                soundClips.appendChild(clipContainer)
-
-                audio.controls = true
-                const blob = new Blob(chunks, {
-                    'type': 'audio/ogg codecs=opus'
-                })
-                console.log(new Uint8Array(blob))
-                chunks = []
-                const audioURL = URL.createObjectURL(blob)
-                audio.src = audioURL
-                console.log("recorder stopped")
-                console.log(audio.src)
-
-                deleteButton.onclick = e => {
-                    evtTgt = e.target
-                    evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode)
-                }
-            }
-
-            mediaRecorder.ondataavailable = e => {
-                chunks.push(e.data)
-            }
-        })
-        .catch(err => {
-            console.log('The following error occurred: ' + err)
-        })
 }
-function createDownloadLink(blob) {
+signup.onclick = async function (event) {
+    console.log("Audio being exported.")
+    var filename = id.value + ".wav";
+    // console.log(filename)
+    console.log(blob)
+    let fd = new FormData();
+    fd.append('audio_data', blob, filename);
+    fd.append('id', id.value);
+    console.log(fd)
 
-    var url = URL.createObjectURL(blob);
-    var au = document.createElement('audio');
-    var li = document.createElement('li');
-    var link = document.createElement('a');
-
-    //name of .wav file to use during upload and download (without extendion)
-    var filename = new Date().toISOString();
-
-    //add controls to the <audio> element
-    au.controls = true;
-    au.src = url;
-
-    //save to disk link
-    link.href = url;
-    link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
-    link.innerHTML = "Save to disk";
-
-    //add the new audio element to li
-    li.appendChild(au);
-
-    //add the filename to the li
-    li.appendChild(document.createTextNode(filename+".wav "))
-
-    //add the save to disk link to li
-    li.appendChild(link);
-
-    //upload link
-    var upload = document.createElement('a');
-    upload.href="#";
-    upload.innerHTML = "Upload";
-    upload.addEventListener("click", function(event){
-          var xhr=new XMLHttpRequest();
-          xhr.onload=function(e) {
-              if(this.readyState === 4) {
-                  console.log("Server returned: ",e.target.responseText);
-              }
-          };
-          var fd=new FormData();
-          fd.append("audio_data",blob, filename);
-          xhr.open("POST","/",true);
-          xhr.send(fd);
-    })
-    li.appendChild(document.createTextNode (" "))//add a space in between
-    li.appendChild(upload)//add the upload link to li
-
-    //add the li element to the ol
-    recordingsList.appendChild(li);
+    try {
+        let r = await fetch('http://127.0.0.1:5000/signup', { method: "POST", body: fd });
+        console.log('HTTP response code:', r.status);
+    } catch (e) {
+        console.log(e);
+    }
 }
